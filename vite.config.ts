c74c3a5da9571ad3b3e7a5afd04449a74f21a968
nicoldissi <91062354+nicoldissi@users.noreply.github.com>
2025-03-04 10:12:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,12 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
   },
   plugins: [
     react(),
