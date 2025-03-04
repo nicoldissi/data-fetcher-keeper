1063f360879fbd9a5e5e643577c9eaa6ebe5f49f
nicoldissi <91062354+nicoldissi@users.noreply.github.com>
2025-03-04 10:21:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -8,11 +7,11 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
-    port: 8080,
+    port: parseInt(process.env.PORT || "8080"),
   },
   preview: {
     host: "0.0.0.0",
-    port: 8080,
+    port: parseInt(process.env.PORT || "8080"),
   },
   plugins: [
     react(),
